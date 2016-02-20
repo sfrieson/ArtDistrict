223cf1b2568103bce65c71dfa6108b3302ac2b9d
Joshua Carrafa <carrafa@gmail.com>
2016-02-20 14:52:57 -0500

    adds map

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,12 @@
 console.log('mapatttackkkk');
+
+var initMapOptions = {
+  center: {
+    lat: 40.732784,
+    lng: -73.975941
+  },
+  scrollwheel: false,
+  zoom: 13
+};
+
+var map = new google.maps.Map(document.getElementById('map'), initMapOptions)
