4a5a53fc891a1d44a3ce9e4cf4f8d91df11e6e25
Joshua Carrafa <carrafa@gmail.com>
2016-02-20 16:13:03 -0500

    refactoring, updates heatmap on submit button

--- public/js/script.js
+++ public/js/script.js
@@ -2,36 +2,37 @@ console.log('mapatttackkkk');
 
 $(function() {
   initMap(initMapOptions);
+  setSubmitHandler();
 });
 
 var initMapOptions = {
   center: {
-    lat: 40.732784,
-    lng: -73.975941
+    lat: 40.678,
+    lng: -73.965
   },
   scrollwheel: false,
-  zoom: 13
+  zoom: 15
 };
 
 var map, heatmap;
 
+// initial map options
 function initMap(mapOptions) {
 
   map = new google.maps.Map(document.getElementById('map'), mapOptions)
   map.mapTypes.set(customMapTypeId, mapStyle);
   map.setMapTypeId(customMapTypeId);
 
-  heatmap = new google.maps.visualization.HeatmapLayer({
-    data: getPoints(),
-    map: map
-  });
+  newHeatMap();
 
 }
 
+// styles gradient
 function changeGradient() {
   heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
 }
 
+// random points for now, will be ajax call eventually
 function getPoints() {
   var heatPointArray = [];
   for (i = 0; i < 100; i++) {
@@ -40,32 +41,23 @@ function getPoints() {
     heatPointArray.push(new google.maps.LatLng(lat, lng))
   }
   return heatPointArray;
-  // return [
-  //   new google.maps.LatLng(40.6784471, -73.9653714),
-  //   new google.maps.LatLng(40.6784471, -73.9753714),
-  //   new google.maps.LatLng(40.6784471, -73.9853714),
-  //   new google.maps.LatLng(40.6784471, -73.9953714),
-  //   new google.maps.LatLng(40.6784471, -73.9453714),
-  //   new google.maps.LatLng(40.6784471, -73.9353714),
-  //   new google.maps.LatLng(40.6784471, -73.9253714),
-  //   new google.maps.LatLng(40.6784471, -73.9153714),
-  //   new google.maps.LatLng(40.6784471, -73.9053714),
-  //   new google.maps.LatLng(40.6784471, -73.8953714),
-  //   new google.maps.LatLng(40.6784471, -73.8853714),
-  //   new google.maps.LatLng(40.6784471, -73.8753714),
-  //   new google.maps.LatLng(40.6784471, -73.8653714),
-  //   new google.maps.LatLng(40.6784471, -73.8553714),
-  //   new google.maps.LatLng(40.6784471, -73.8453714),
-  //   new google.maps.LatLng(40.6784471, -73.8353714),
-  //   new google.maps.LatLng(40.6784471, -73.8253714),
-  //   new google.maps.LatLng(40.6784471, -73.8153714),
-  //   new google.maps.LatLng(40.6784471, -73.8053714),
-  //   new google.maps.LatLng(40.6784471, -73.7953714),
-  //   new google.maps.LatLng(40.6784471, -73.7853714),
-  //   new google.maps.LatLng(40.6784471, -73.7753714),
-  //   new google.maps.LatLng(40.6784471, -73.7653714),
-  //   new google.maps.LatLng(40.6784471, -73.7565371),
-  //   new google.maps.LatLng(40.6784471, -73.7453714),
-  //   new google.maps.LatLng(40.6784471, -73.7353714)
-  // ]
+}
+
+// this will, update the heatpoints
+function setSubmitHandler() {
+  $('#submit').click(function(e) {
+    e.preventDefault();
+    newHeatMap();
+  });
+}
+
+// this clears out the old heatmap and renders a new one.  eventually will be based on new data.
+function newHeatMap() {
+  if (heatmap) {
+    heatmap.setMap(null);
+  }
+  heatmap = new google.maps.visualization.HeatmapLayer({
+    data: getPoints(),
+    map: map
+  });
 }
