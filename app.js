8f18229c02eaa8ff7ef1f3ca5d0c7e065519e090
Aleksandra Matiyev <aleksa000777@mail.ru>
2016-02-20 13:08:55 -0500

    search yelp

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 require('dotenv').load();
+var express = require('express');
+var router = express.Router();
 var Yelp = require('yelp');
 
 
@@ -8,3 +10,13 @@ var yelp = new Yelp({
   token: process.env.TOKEN,
   token_secret: process.env.TOKENSECRET
 });
+
+router.get('/search', function(req,res){
+    yelp.search({ term:'',category_filter: 'museums', location: req.query.location || "new+york", sort:"0"})
+    .then(function(data){
+      console.log(data, 'data');
+      res.json(data)
+    })
+    .catch(function(err){
+      console.log(err);
+    })
