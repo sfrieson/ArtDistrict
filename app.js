3027cb4046c501106447132bfb87501a2cc8fe68
Aleksandra Matiyev <aleksa000777@mail.ru>
2016-02-20 12:09:17 -0500

    yelp

---
+++ app.js
@@ -0,0 +1,9 @@
+require('dotenv').load()
+
+
+var yelp = new Yelp({
+  consumer_key: process.env.CONSUMERKEY,
+  consumer_secret: process.env.CONSUMERSECRET,
+  token: process.env.TOKEN,
+  token_secret: process.env.TOKENSECRET
+});
