8628479c678d036ab8eacd42bc7fb5164e21e2e3
Joshua Carrafa <carrafa@gmail.com>
2016-02-20 15:52:42 -0500

    adds random heatpoint generator

--- public/js/script.js
+++ public/js/script.js
@@ -33,32 +33,39 @@ function changeGradient() {
 }
 
 function getPoints() {
-  return [
-    new google.maps.LatLng(40.6784471, -73.9653714),
-    new google.maps.LatLng(40.6784471, -73.9753714),
-    new google.maps.LatLng(40.6784471, -73.9853714),
-    new google.maps.LatLng(40.6784471, -73.9953714),
-    new google.maps.LatLng(40.6784471, -73.9453714),
-    new google.maps.LatLng(40.6784471, -73.9353714),
-    new google.maps.LatLng(40.6784471, -73.9253714),
-    new google.maps.LatLng(40.6784471, -73.9153714),
-    new google.maps.LatLng(40.6784471, -73.9053714),
-    new google.maps.LatLng(40.6784471, -73.8953714),
-    new google.maps.LatLng(40.6784471, -73.8853714),
-    new google.maps.LatLng(40.6784471, -73.8753714),
-    new google.maps.LatLng(40.6784471, -73.8653714),
-    new google.maps.LatLng(40.6784471, -73.8553714),
-    new google.maps.LatLng(40.6784471, -73.8453714),
-    new google.maps.LatLng(40.6784471, -73.8353714),
-    new google.maps.LatLng(40.6784471, -73.8253714),
-    new google.maps.LatLng(40.6784471, -73.8153714),
-    new google.maps.LatLng(40.6784471, -73.8053714),
-    new google.maps.LatLng(40.6784471, -73.7953714),
-    new google.maps.LatLng(40.6784471, -73.7853714),
-    new google.maps.LatLng(40.6784471, -73.7753714),
-    new google.maps.LatLng(40.6784471, -73.7653714),
-    new google.maps.LatLng(40.6784471, -73.7565371),
-    new google.maps.LatLng(40.6784471, -73.7453714),
-    new google.maps.LatLng(40.6784471, -73.7353714)
-  ]
+  var heatPointArray = [];
+  for (i = 0; i < 100; i++) {
+    lat = 40.678 + Math.random() * 0.01
+    lng = -73.965 + Math.random() * 0.01
+    heatPointArray.push(new google.maps.LatLng(lat, lng))
+  }
+  return heatPointArray;
+  // return [
+  //   new google.maps.LatLng(40.6784471, -73.9653714),
+  //   new google.maps.LatLng(40.6784471, -73.9753714),
+  //   new google.maps.LatLng(40.6784471, -73.9853714),
+  //   new google.maps.LatLng(40.6784471, -73.9953714),
+  //   new google.maps.LatLng(40.6784471, -73.9453714),
+  //   new google.maps.LatLng(40.6784471, -73.9353714),
+  //   new google.maps.LatLng(40.6784471, -73.9253714),
+  //   new google.maps.LatLng(40.6784471, -73.9153714),
+  //   new google.maps.LatLng(40.6784471, -73.9053714),
+  //   new google.maps.LatLng(40.6784471, -73.8953714),
+  //   new google.maps.LatLng(40.6784471, -73.8853714),
+  //   new google.maps.LatLng(40.6784471, -73.8753714),
+  //   new google.maps.LatLng(40.6784471, -73.8653714),
+  //   new google.maps.LatLng(40.6784471, -73.8553714),
+  //   new google.maps.LatLng(40.6784471, -73.8453714),
+  //   new google.maps.LatLng(40.6784471, -73.8353714),
+  //   new google.maps.LatLng(40.6784471, -73.8253714),
+  //   new google.maps.LatLng(40.6784471, -73.8153714),
+  //   new google.maps.LatLng(40.6784471, -73.8053714),
+  //   new google.maps.LatLng(40.6784471, -73.7953714),
+  //   new google.maps.LatLng(40.6784471, -73.7853714),
+  //   new google.maps.LatLng(40.6784471, -73.7753714),
+  //   new google.maps.LatLng(40.6784471, -73.7653714),
+  //   new google.maps.LatLng(40.6784471, -73.7565371),
+  //   new google.maps.LatLng(40.6784471, -73.7453714),
+  //   new google.maps.LatLng(40.6784471, -73.7353714)
+  // ]
 }
